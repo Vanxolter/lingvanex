29fc8685ffa576381ac3c4f24385140196536e80
vanxolter <Vansheltor@gmail.com>
2023-01-31 22:25:01 +0300

    add sort by year

--- parseapp/main/views.py
+++ parseapp/main/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from main.forms import YearFilterForm
 from main.models import Parse_data
+from main.queries import filter_years
 
 
 def main_page(request):
-    context = Parse_data.objects.all()
-    paginator = Paginator(context, 10)  # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        apps = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        apps = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        apps = paginator.page(paginator.num_pages)
-    return render(request,
-                  'main_page.html',
-                  {'page': page,
-                   'apps': apps,
-                   })
+    all_apps = Parse_data.objects.all()
+
+    filters_form = YearFilterForm(request.GET)
+    if filters_form.is_valid():
+        order_by = filters_form.cleaned_data["order_by"]
+        context = filter_years(all_apps, order_by)
+
+        paginator = Paginator(context, 10)
+        page = request.GET.get('page')
+
+        try:
+            apps = paginator.page(page)
+        except PageNotAnInteger:
+            apps = paginator.page(1)
+        except EmptyPage:
+            apps = paginator.page(paginator.num_pages)
+
+        return render(request,
+                      'main_page.html',
+                      {'page': page,
+                       'apps': apps,
+                       "filters_form": filters_form,
+                       })
